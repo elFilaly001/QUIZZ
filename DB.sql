5b3fdfca005b04fed90c5dbbf5c696cbfa0611f2
Anass-Dr <85074995+Anass-Dr@users.noreply.github.com>
2023-12-11 11:26:31 +0100

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,55 +1,283 @@
 Create database quizz ; 
 
-Create table users (
-    user_id int auto_increment Primary key ,
-    user_name varchar(50),
-    user_email varchar(50),
-    user_password varchar(255);
-    user_role boolean 
-)
+CREATE TABLE `answers` (
+  `answer_id` int(11) NOT NULL,
+  `answer_content` varchar(255) DEFAULT NULL,
+  `answer_is_correct` tinyint(1) DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-Create table courses (
-    course_id int auto_increment Primary key , 
-    coure_title varchar(50),
-    course_description varchar(25000)
-)
+--
+-- Dumping data for table `answers`
+--
 
-Create table course_progress(
-    progress_index  int ,
-    course_id int ,
-    user_id int ,
-    Primary key (course_id , user_id),
-    Constraint Course_progress_course_id foreign key (course_id)  references courses(course_id) on delete cascade on update cascade ,
-    Constraint Course_progress_user_id foreign key (user_id)  references users(users_id) on delete cascade on update cascade
-)
+INSERT INTO `answers` (`answer_id`, `answer_content`, `answer_is_correct`, `question_id`) VALUES
+(41, 'app1', 0, 11),
+(42, 'app2', 1, 11),
+(43, 'app3', 0, 11),
+(44, 'app4', 0, 11),
+(45, 'app1', 0, 12),
+(46, 'app2', 1, 12),
+(47, 'app3', 0, 12),
+(48, 'app4', 0, 12);
 
-Create table quizz (
-    quizz_id int auto_increment primary key ,
-    quizz_title varchar(50),
-    course_id int ,
-    Constraint quizz_course_id foreign key (course_id)  references courses(course_id) on delete cascade on update cascade
-)
+-- --------------------------------------------------------
 
-create table quizz_progress (
-    quizz_progress int auto_increment Primary key,
-    quizz_score int ,
-    quizz_correct_answers int , 
-    quizz_date date , 
-    quizz_id int , 
-    Constraint quizz_quizz_id foreign key (quizz_id)  references quizz(quizz_id) on delete cascade on update cascade
-)
+--
+-- Table structure for table `course_progress`
+--
 
-create table question (
-    question_id int auto_increment Primary key,
-    question_content varchar(500),
-    quizz_id int ,
-    Constraint question_quizz_id foreign key (quizz_id)  references quizz(quizz_id) on delete cascade on update cascade
-)
+CREATE TABLE `course_progress` (
+  `progress_index` int(11) DEFAULT '0',
+  `course_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-create table answers (
-    answer_id int auto_increment Primary key,
-    answer_content varchar(255),
-    answer_is_correct boolean,
-    question_id int , 
-    Constraint answer_question_id foreign key (question_id)  references quizz_question(question_id) on delete cascade on update cascade
-)
+--
+-- Dumping data for table `course_progress`
+--
+
+INSERT INTO `course_progress` (`progress_index`, `course_id`, `user_id`) VALUES
+(0, 1, 1),
+(297, 1, 2),
+(0, 1, 3),
+(182, 2, 2),
+(0, 3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `course_id` int(11) NOT NULL,
+  `course_title` varchar(50) DEFAULT NULL,
+  `course_description` varchar(25000) DEFAULT NULL,
+  `course_duration` varchar(20) NOT NULL,
+  `course_creation_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`course_id`, `course_title`, `course_description`, `course_duration`, `course_creation_date`) VALUES
+(1, 'PHP', '<h4>What is Lorem Ipsum?</h4>\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n<br/>\r\n\r\n<h4>Why do we use it?</h4>\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n<br/>\r\n\r\n\r\n<h4>Where does it come from?</h4>\r\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\r\n\r\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\r\n<br/>\r\n\r\n<h4>Where can I get some?</h4>\r\nThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', '1 Week', '2023-12-01'),
+(2, 'JavaScript', '<h4>What is Lorem Ipsum?</h4>\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n<br/>\r\n\r\n<h4>Why do we use it?</h4>\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n<br/>\r\n\r\n\r\n<h4>Where does it come from?</h4>\r\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\r\n\r\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\r\n<br/>\r\n\r\n<h4>Where can I get some?</h4>\r\nThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', '1 Month', '2023-12-03'),
+(3, 'Laravel', '<h4>What is Lorem Ipsum?</h4>\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n<br/>\r\n\r\n<h4>Why do we use it?</h4>\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n<br/>\r\n\r\n\r\n<h4>Where does it come from?</h4>\r\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\r\n\r\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\r\n<br/>\r\n\r\n<h4>Where can I get some?</h4>\r\nThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', '2 Months', '2023-12-05'),
+(10, 'Course 20', 'ccccccc', '2 weeks', '2023-12-10'),
+(11, 'test curr date ', 'aaa', '3 weeks', '2023-12-11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE `question` (
+  `question_id` int(11) NOT NULL,
+  `question_content` varchar(500) DEFAULT NULL,
+  `quizz_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`question_id`, `question_content`, `quizz_id`) VALUES
+(11, 'appaa1', 32),
+(12, 'appaa2', 32);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizz`
+--
+
+CREATE TABLE `quizz` (
+  `quizz_id` int(11) NOT NULL,
+  `quizz_title` varchar(50) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `quizz`
+--
+
+INSERT INTO `quizz` (`quizz_id`, `quizz_title`, `course_id`) VALUES
+(32, 'appah', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizz_progress`
+--
+
+CREATE TABLE `quizz_progress` (
+  `quizz_progress` int(11) NOT NULL,
+  `quizz_score` int(11) DEFAULT NULL,
+  `quizz_correct_answers` int(11) DEFAULT NULL,
+  `quizz_date` date DEFAULT NULL,
+  `quizz_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `quizz_progress`
+--
+
+INSERT INTO `quizz_progress` (`quizz_progress`, `quizz_score`, `quizz_correct_answers`, `quizz_date`, `quizz_id`) VALUES
+(1, 10, 1, '2023-12-09', 32),
+(2, 10, 1, '2023-12-10', 32),
+(3, 0, 0, '2023-12-11', 32),
+(4, 0, 0, '2023-12-11', 32),
+(5, 0, 0, '2023-12-11', 32);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `user_email` varchar(50) DEFAULT NULL,
+  `user_password` varchar(255) DEFAULT NULL,
+  `user_role` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `user_role`) VALUES
+(1, 'admin', 'admin@gmail.com', '123', 1),
+(2, 'anass', 'anass@gmail.com', '123', 0),
+(3, 'abdo', 'abdo@gmail.com', '123', 0),
+(4, 'abdou', 'abdou@gmail.com', '$2y$10$.1vkQ8VOQZOB3qJeCoY1KuVRKWgLZACNeQsVw5pg7zCh8r/2pm6le', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`answer_id`),
+  ADD KEY `answer_question_id` (`question_id`);
+
+--
+-- Indexes for table `course_progress`
+--
+ALTER TABLE `course_progress`
+  ADD PRIMARY KEY (`course_id`,`user_id`),
+  ADD KEY `Course_progress_user_id` (`user_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`question_id`),
+  ADD KEY `question_quizz_id` (`quizz_id`);
+
+--
+-- Indexes for table `quizz`
+--
+ALTER TABLE `quizz`
+  ADD PRIMARY KEY (`quizz_id`),
+  ADD KEY `quizz_course_id` (`course_id`);
+
+--
+-- Indexes for table `quizz_progress`
+--
+ALTER TABLE `quizz_progress`
+  ADD PRIMARY KEY (`quizz_progress`),
+  ADD KEY `quizz_quizz_id` (`quizz_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `quizz`
+--
+ALTER TABLE `quizz`
+  MODIFY `quizz_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+--
+-- AUTO_INCREMENT for table `quizz_progress`
+--
+ALTER TABLE `quizz_progress`
+  MODIFY `quizz_progress` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `answers`
+--
+ALTER TABLE `answers`
+  ADD CONSTRAINT `answer_question_id` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `course_progress`
+--
+ALTER TABLE `course_progress`
+  ADD CONSTRAINT `Course_progress_course_id` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Course_progress_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `question`
+--
+ALTER TABLE `question`
+  ADD CONSTRAINT `question_quizz_id` FOREIGN KEY (`quizz_id`) REFERENCES `quizz` (`quizz_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `quizz`
+--
+ALTER TABLE `quizz`
+  ADD CONSTRAINT `quizz_course_id` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `quizz_progress`
+--
+ALTER TABLE `quizz_progress`
+  ADD CONSTRAINT `quizz_quizz_id` FOREIGN KEY (`quizz_id`) REFERENCES `quizz` (`quizz_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
